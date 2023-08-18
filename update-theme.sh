1adea58095b01e9703845af582072312f87b59fc
shadab@8848digital.com <shadab@8848digital.com>
2023-08-18 10:47:45 +0530

    quick order,dealer-ledger page added:sk

--- update-theme.sh
+++ update-theme.sh
@@ -3,6 +3,8 @@ rm -r components
 rm -r styles
 rm -r _app.tsx
 rm -r index.tsx
+rm -r quick-order.tsx
+rm -r dealer-ledger.tsx
 rm -r assets
 
 cp -R ../../cards .
@@ -10,4 +12,6 @@ cp -R ../../components .
 cp -R ../../styles .
 cp -R ../../pages/_app.tsx .
 cp -R ../../pages/index.tsx .
+cp -R ../../pages/quick-order.tsx .
+cp -R ../../pages/dealer-ledger.tsx .
 cp -R ../../public/assets .
